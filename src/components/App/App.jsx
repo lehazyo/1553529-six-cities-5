ca17251095182346d9c39a01f86697687730df44
Алексей Сергиенко <lehazyo@gmail.com>
2020-10-04 18:45:31 +0300

    Задание 7

---
+++ src/components/App/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Main from "../Main/Main";
+import Sign from "../Sign/Sign";
+import Favorites from "../Favorites/Favorites";
+import Offer from "../Offer/Offer";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route exact path="/login">
+          <Sign />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:offerId">
+          <Offer />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
