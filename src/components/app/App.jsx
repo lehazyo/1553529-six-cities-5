65b8313e1608867a2b68ad06edaa881725f30705
Алексей Сергиенко <lehazyo@gmail.com>
2020-10-04 12:56:14 +0300

    Урок 1 / Задание 5 / Коммит 1

---
+++ src/components/app/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Title from "../title/Title";
+
+const App = (props) => {
+
+  return (
+    <Title />
+  );
+};
+
+export default App;
