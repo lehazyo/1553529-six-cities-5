731e92a08ce936439bc45d5a7462241041f5cfe4
Алексей Сергиенко <lehazyo@gmail.com>
2020-11-15 15:12:17 +0300

    Задание 12 / Коммит 2

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,30 +1,52 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Review = (props) => {
+  let parsedDate = Date.parse(props.review.datetime);
+  parsedDate = new Date(parsedDate);
+  const monthNames = [`Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `Jul`, `Aug`, `Sep`, `Oct`, `Nov`, `Dev`];
+  const dateMonthYear = monthNames[parsedDate.getMonth()] + ` ` + parsedDate.getFullYear();
+
   return (
     <li className="reviews__item">
       <div className="reviews__user user">
         <div className="reviews__avatar-wrapper user__avatar-wrapper">
-          <img className="reviews__avatar user__avatar" src="/img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
+          <img
+            className="reviews__avatar user__avatar"
+            src={props.review.avatarUrl}
+            width="54"
+            height="54"
+            alt="Reviews avatar"
+          />
         </div>
         <span className="reviews__user-name">
-          Max
+          {props.review.author}
         </span>
       </div>
       <div className="reviews__info">
         <div className="reviews__rating rating">
           <div className="reviews__stars rating__stars">
-            <span style="width: 80%"></span>
+            <span style={{width: ((props.review.rating / 5) * 100).toString() + `%`}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <p className="reviews__text">
-          A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
+          {props.review.text}
         </p>
-        <time className="reviews__time" datetime="2019-04-24">April 2019</time>
+        <time className="reviews__time" dateTime={props.review.datetime}>{ dateMonthYear }</time>
       </div>
     </li>
   );
+};
+
+Review.propTypes = {
+  review: PropTypes.shape({
+    avatarUrl: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    datetime: PropTypes.string.isRequired
+  })
 };
 
 export default Review;
--- src/components/ReviewsList/ReviewsList.jsx
+++ src/components/ReviewsList/ReviewsList.jsx
@@ -3,20 +3,21 @@ import CommentForm from "../CommentForm/CommentForm";
 import Review from "../Review/Review";
 import reviews from "../../mocks/reviews.json";
 
-const ReviewsList = (props) => {
+const ReviewsList = () => {
   return (
     <section className="property__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ul className="reviews__list">
-        {reviews.map((review) => {
+        {reviews.map((review) =>
           <Review
+            key={review.id}
             review={review}
           />
-        })}
+        )}
       </ul>
       <CommentForm />
     </section>
-  )
+  );
 };
 
 export default ReviewsList;
